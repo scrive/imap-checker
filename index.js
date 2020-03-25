a295d12ce274a1abcd87e6635802fc8a006a4eb8
Clara Attermo <clara.attermo@scrive.com>
2020-03-25 14:22:52 +0100

    Fix some bugs (#1)
    * Only resolve if matched
    * Simplified timeout
    * Timeout in ms

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-
 const mailNotifier = require('mail-notifier');
 
-const imapChecker = (imapConfig, { timeout, from, to, subject }) => {
-  return new Promise((resolve, reject) => {
+const imapChecker = (imapConfig, {timeout, from, to, subject}) => {
+  return new Promise((resolve) => {
     const notifier = mailNotifier(imapConfig);
+    let timeoutFunc;
 
-    const startTime = Date.now();
-    let found = false;
-
-    notifier.on('end', () => {
-      var currentTime = Date.now()
-      if (currentTime - startTime > timeout * 1000 ) {
-        resolve(null);
-      } else if (!found) {
-        notifier.start();
-      }
-    })
-    .on('mail', mail => {
-      let matched = true;
-      if (from && ! mail.from.find(element => element.address === from)) {
-        matched = false;
-      }
-      if (to && ! mail.to.find(element => element.address === to)) {
-        matched = false;
-      }
-      if (subject && ! mail.subject.match(new RegExp(subject))) {
-        matched = false;
-      }
-      found = matched ? true : false;
-      resolve(mail);
-    })
-    .on('connected', () => {
-      setTimeout(() => {
-        notifier.stop();
-      }, 2000)
-    })
-    .on('error', err => console.log('An error has occured:', err))
-    .start();
-  })
-}
+    notifier
+      .on('mail', (mail) => {
+        let matched = true;
+        if (from && !mail.from.some((element) => element.address === from)) {
+          matched = false;
+        }
+        if (to && !mail.to.some((element) => element.address === to)) {
+          matched = false;
+        }
+        if (subject && !mail.subject.match(new RegExp(subject))) {
+          matched = false;
+        }
+        if (matched) {
+          clearTimeout(timeoutFunc);
+          notifier.stop();
+          resolve(mail);
+        }
+      })
+      .on('connected', () => {
+        timeoutFunc = setTimeout(() => {
+          notifier.stop();
+          console.log(`Timeout when checking for email from: '${from}' to: '${to}' with subject: '${subject}'`);
+          resolve(null);
+        }, timeout);
+      })
+      .on('error', (err) => {
+        console.log(`Error when checking for email: ${err.message} \n\n ${err.stack}`);
+      })
+      .start();
+  });
+};
 
 exports.checkMail = imapChecker;
