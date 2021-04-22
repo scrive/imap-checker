a967a5b53767ba6f63ad7e3ad0cacbe144c722e0
Daniel Han <daniel.han@scrive.com>
2021-04-22 22:10:48 +0200

    Code refactoring

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
-const mailNotifier = require('mail-notifier');
+import imaps from 'imap-simple';
+import { simpleParser } from 'mailparser';
+import _ from 'lodash';
 
-const imapChecker = (imapConfig, {timeout, from, to, subject}) => {
-  return new Promise((resolve, reject) => {
-    const notifier = mailNotifier(imapConfig);
-    let timeoutFunc;
+const sleep = ms => new Promise((resolve) => setTimeout(resolve, ms));
 
-    notifier
-      .on('mail', (mail) => {
-        let matched = true;
-        if (from && !mail.from.some((element) => element.address === from)) {
-          matched = false;
-        }
-        if (to && !mail.to.some((element) => element.address === to)) {
-          matched = false;
-        }
-        if (subject && !mail.subject.match(new RegExp(subject))) {
-          matched = false;
-        }
-        if (matched) {
-          clearTimeout(timeoutFunc);
-          notifier.stop();
-          resolve(mail);
-        }
-      })
-      .on('connected', () => {
-        timeoutFunc = setTimeout(() => {
-          notifier.stop();
-          console.log(`Timeout when checking for email from: '${from}' to: '${to}' with subject: '${subject}'`);
-          resolve(null);
-        }, timeout);
-      })
-      .on('error', (err) => {
-        console.log(`Error when checking for email: ${err.message} \n\n ${err.stack}`);
-        reject(err);
-      })
-      .start();
+const parse = (source, options) => {
+  return new Promise(resolve => {
+    simpleParser(source, options, (err, parsed) => resolve(parsed));
   });
+}
+
+const imapChecker = async (imapConfig, {timeout, from, to, subject}) => {
+    const config = {
+      imap: imapConfig
+    };
+
+    const connection = await imaps.connect(config);
+    await connection.openBox('INBOX');
+
+    const searchCriteria = ['UNSEEN'];
+    const fetchOptions = { bodies: ['HEADER', 'TEXT', ''] };
+
+    const startTime = Date.now();
+
+    while (true) {
+      const messages = await connection.search(searchCriteria, fetchOptions);
+
+      for (let index = 0; index < messages.length; index++) {
+        const message = messages[index];
+        const all = _.find(message.parts, { "which": "" })
+        const id = message.attributes.uid;
+        const idHeader = "Imap-Id: " + id + "\r\n";
+
+        const mail = await parse(idHeader + all.body);
+
+        let matched = true;
+        if (from && from != mail.from.text) {
+          matched = false;
+        }
+
+        if (to && to != mail.to.text) {
+          matched = false;
+        }
+
+        if (subject && subject != mail.subject) {
+          matched = false;
+        }
+
+        if (matched) {
+          return mail;
+        }
+      }
+
+      const now = Date.now();
+      if (now - startTime > timeout) {
+        throw new Error(`TimeoutError: couln't find emails in ${timeout}ms.`);
+      }
+
+      await sleep(1000);
+    }
 };
 
 exports.checkMail = imapChecker;
